7462b94473dd985af4e532c62bddbd1ecc07ab4e
samruddhibhoyar3 <skbhoyar36.sb@gmail.com>
2025-09-25 13:32:25 +0530

    user-service-folder-structure

---
+++ UserService/src/test/java/portal/elearning/UserService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package portal.elearning.UserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
